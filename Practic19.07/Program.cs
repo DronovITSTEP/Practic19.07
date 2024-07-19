5b01cc6a85a627dcd3fe649b1d2ff973d7f06ce8
ApxNord <dronov_a@t.top-academy.ru>
2024-07-19 20:52:41 +0300

    Добавьте файлы проекта.

---
+++ Practic19.07/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic19._07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Title = "Пример моего заголовка консоли";
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.SetWindowSize(150, 10);
+
+            Console.WriteLine("Это моя первая строка");
+
+            Console.ResetColor();
+
+            Console.WriteLine("Клавиша NumLock нажата? -> " + Console.NumberLock);
+            Console.WriteLine("Клавиша CapsLock нажата? -> " + Console.CapsLock.ToString());*/
+
+            /*byte, short, int, long;
+            float, double, decimal;
+            bool;
+            string;
+            char;*/
+
+            /*int num = int.Parse(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num.ToString());*/
+
+            /*Console.WriteLine("num1 * num2 = " + 
+                int.Parse(Console.ReadLine()) *
+                int.Parse(Console.ReadLine()));*/
+
+            /*int i = 7;
+            Console.WriteLine(i == 4 ? "yes" : "no");*/
+
+            /*Console.WriteLine("числа -> {1}, {1}, {0}",
+                5, 15, 13);*/
+
+            /*int n1 = 4, n2 = 8, n3 = 1;
+            Console.WriteLine($"числа {n1},{n2},{n3}");*/
+
+            Console.ReadKey();
+        }
+    }
+}
